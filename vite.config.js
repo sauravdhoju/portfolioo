abbdd643f3cd1f28bd13add2e4c5cb270acfc57e
sauravdhoju <122729823+sauravdhoju@users.noreply.github.com>
2025-08-04 12:47:31 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/portfolioo/',
+    base: '/',
     plugins: [react()],
     build: {
         rollupOptions: {
