14a83bea7fddff80fef44e23bfd4328472e11849
Saurav Dhoju <sauravdhoju12@gmail.com>
2026-07-02 23:15:13 +0545

    fix: suppress SCSS if-function deprecation warnings from Bootstrap

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
+                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import", "if-function"],
             },
         },
     },
